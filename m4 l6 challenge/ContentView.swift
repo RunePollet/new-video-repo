d27835715e808e5f3d5f3552d004916dd2aeedad
Rune Pollet <runepollet@icloud.com>
2022-02-18 15:56:36 +0100

    Initial Commit

---
+++ m4 l6 challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  m4 l6 challenge
+//
+//  Created by Rune Pollet on 18/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ m4 l6 challenge/m4_l6_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  m4_l6_challengeApp.swift
+//  m4 l6 challenge
+//
+//  Created by Rune Pollet on 18/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct m4_l6_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
